c5e3517d0adc31344e577a4cf73d3b0561d0ff5c
tkmagesh <tkmagesh77@gmail.com>
2015-02-20 14:05:39 +0530

    Server Sent Events

--- Day-02/01-WebSockets/CiscoSocketServer/chatServer.js
+++ Day-02/01-WebSockets/CiscoSocketServer/chatServer.js
@@ -1,10 +1,16 @@
 var nodeJsWebSocket = require("nodejs-websocket");
 var socketServer = nodeJsWebSocket.createServer(function(connection){
 	console.log("A new connection is established");
+	connection.on("close", function(){
+		console.log("connection closed");
+	});
+	connection.on("error", function(err){
+		console.log(err);
+	});
 	connection.on("text", function(msg){
 		for(var i=0; i<socketServer.connections.length; i++){
 			var clientConnection = socketServer.connections[i];
-			if (clientConnection !== connection)
+			if (clientConnection !== connection && clientConnection.readyState === clientConnection.OPEN)
 				clientConnection.sendText(msg);
 		}
 	});
